02e9f05fd7bc386ae7a72d360da7babd482481a4
curtbansil <22698418+kurtis-b@users.noreply.github.com>
2025-03-14 23:45:48 -0700

    Added counting of calls to PU

--- parse_mem_ctrl_cmds.py
+++ parse_mem_ctrl_cmds.py
@@ -5,9 +5,9 @@ from collections import OrderedDict
 def count_substrings(file_path, unique_substrings):
     modes = ["HAB", "HAB mode", "HAB_PIM", "SB mode"]
     substring_count = OrderedDict((mode, {"invoke_count": 0, "substrings": {}}) for mode in modes)
-    current_mode = "Setting up banks"
+    current_mode = "Setting up banks (Not a command)"
     substring_count[current_mode] = {"invoke_count": 0, "substrings": {}}
-    modes_order = ["Setting up banks"]
+    modes_order = ["Setting up banks (Not a command)"]
 
     try:
         with open(file_path, 'r') as file:
@@ -29,6 +29,19 @@ def count_substrings(file_path, unique_substrings):
                             if substring not in substring_count[current_mode]["substrings"]:
                                 substring_count[current_mode]["substrings"][substring] = 0
                             substring_count[current_mode]["substrings"][substring] += 1
+                # Check if a processing unit command is called--"[CMD]" will be in the line
+                if "[CMD]" in line:
+                    pu_cmd = "PU (not a command)"
+                    start_idx = line.find('(') + 1
+                    end_idx = line.find(')', start_idx)
+                    if start_idx > 0 and end_idx > start_idx:
+                        exact_cmd = line[start_idx:end_idx]
+                    if pu_cmd not in substring_count[current_mode]["substrings"]:
+                        substring_count[current_mode]["substrings"][pu_cmd] = {exact_cmd: 0}
+                    else:
+                        if exact_cmd not in substring_count[current_mode]["substrings"][pu_cmd]:
+                            substring_count[current_mode]["substrings"][pu_cmd][exact_cmd] = 0
+                    substring_count[current_mode]["substrings"][pu_cmd][exact_cmd] += 1
     except FileNotFoundError:
         print(f"File not found: {file_path}")
         return
@@ -69,7 +82,7 @@ if __name__ == "__main__":
         os.makedirs(output_dir, exist_ok=True)
         output_file_path = os.path.join(output_dir, "mem_ctrl_cmd_counts.txt")
         with open(output_file_path, 'w') as output_file:
-            output_file.write(f"Total counts found in order as found in {file_path}\n\n")
+            output_file.write(f"Total counts of the commands executed after changing the mode:\n\n")
             for idx, key in enumerate(substring_count):
                 output_file.write(f"{idx}--{key}:\n")
                 output_file.write(f"  TOTAL TIMES INVOKED: {substring_count[key]['invoke_count']}\n")
