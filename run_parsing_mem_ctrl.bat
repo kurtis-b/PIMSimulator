9dd4eb53db86be75497623dec41005d8e32b7971
curtbansil <22698418+kurtis-b@users.noreply.github.com>
2025-03-17 19:48:02 -0700

    Filling in 1 ch examples for rows 64-256 and cols 256-2048

--- run_parsing_mem_ctrl.bat
+++ run_parsing_mem_ctrl.bat
@@ -2,23 +2,20 @@
 setlocal enabledelayedexpansion
 
 REM List of values to pass to the script
-set values=0
 set input_dim=256 512 1024 2048
 set output_dim=64 128 256
 
 REM Loop through each value and run the script in the background
-for %%a in (%values%) do (
-    for %%b in (%input_dim%) do (
-        for %%c in (%output_dim%) do (
-            set "filename=pim_trace_1ch_gemv_%%cx%%b.out"
-            start /B python parse_animate_faster.py %%a !filename! %%b %%c
-        )
+for %%b in (%input_dim%) do (
+    for %%c in (%output_dim%) do (
+        set "filename=pim_trace_1ch_gemv_%%cx%%b.out"
+        start /B python parse_mem_ctrl_cmds.py !filename!
     )
 )
 
 REM Wait for all background processes to finish
 :waitloop
-tasklist | find /I "parse_animate_faster.py" >nul 2>&1
+tasklist | find /I "parse_mem_ctrl_cmds.py" >nul 2>&1
 if not errorlevel 1 (
     timeout /T 1 /NOBREAK >nul
     goto waitloop
